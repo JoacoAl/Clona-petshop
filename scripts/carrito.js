25308aca09088e839c95f00a0112e44cc6a36d2d
chaquito7 <nicolsgonza765@gmail.com>
2023-05-25 01:22:18 -0300

    nicolas-2 V-1

--- scripts/carrito.js
+++ scripts/carrito.js
@@ -1,1 +1,28 @@
 const {createApp} = Vue
+
+const app = createApp({
+    data(){
+        return{
+            allEvents: [],
+            seleccionadas: [],
+        }
+    },
+    created(){
+        fetch("https://mindhub-xj03.onrender.com/api/petshop")
+        .then((res) => res.json())
+        .then((data) => {
+          this.allEvents = data;
+        })
+        .catch((error) => console.error(error)); 
+    },
+    methods: {
+        toggleSeleccion(id){
+            if(this.seleccionadas.find(e => e._id == _id)){
+                console.log("ya esta")
+            }else{
+                const aux = this.allEvents.find(e => e._id == _id)
+                this.seleccionadas.push( aux )
+            }
+        }
+    }
+})
