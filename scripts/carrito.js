1cb8047229e54ca348053e057028d58ece58b2db
chaquito7 <nicolsgonza765@gmail.com>
2023-05-25 12:37:27 -0300

    NICO-3 V-1

--- scripts/carrito.js
+++ scripts/carrito.js
@@ -1,30 +1,40 @@
 const {createApp} = Vue
 
 const app = createApp({
-    data(){
-        return{
-            allEvents: [],
-            seleccionadas: [],
-            mostrarImagen: false,
-            imagenBorrar: false,
-        }
+    data() {
+      return {
+        allEvents: [],
+        seleccionadas: [],
+        mostrarImagen: false,
+        imagenBorrar: false,
+      };
     },
-    created(){
-        this.seleccionadas = JSON.parse(localStorage.getItem('compras')) ?? []
+    created() {
+      this.seleccionadas = JSON.parse(localStorage.getItem('compras')) ?? [];
     },
     methods: {
-        comprar(){
-            localStorage.removeItem('compras')
-            this.seleccionadas = []
-            this.mostrarImagen = true
-        },
-        deleteCompras(){
-            localStorage.removeItem('compras')
-            this.seleccionadas = []
-            this.imagenBorrar = true
+      comprar() {
+        localStorage.removeItem('compras');
+        this.seleccionadas = [];
+        this.mostrarImagen = true;
+      },
+      deleteCompras() {
+        localStorage.removeItem('compras');
+        this.seleccionadas = [];
+        this.imagenBorrar = true;
+      },
+      restar(articulo) {
+        if (articulo.__v > 0) {
+          articulo.__v--;
         }
-    }
-
-})
-
-app.mount('#app')
+      },
+      sumar(articulo) {
+        if (articulo.__v < articulo.disponibles) {
+          articulo.__v++;
+        }
+      },
+    },
+  });
+  
+  app.mount('#app');
+  
